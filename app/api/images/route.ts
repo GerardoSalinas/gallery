2da3fbe9ef915cf0287c165907023e5a67f14838
Gerardo Salinas <gerardoaplicano16@gmail.com>
2025-09-25 09:53:31 -0600

    Removes unused code

--- app/api/images/route.ts
+++ app/api/images/route.ts
@@ -26,14 +26,3 @@ export async function GET(request: Request) {
 
 }
 
-export async function POST(request: Request) {
-  const body = await request.json();
-  const { name } = body;
-
-  const newUser = { id: Date.now(), name };
-
-  return new Response(JSON.stringify(newUser), {
-    status: 201,
-    headers: { "Content-Type": "application/json" },
-  });
-}
